e93f02234707bf76d710e2a5cbf3d3358131b99b
Robert Ylitalo <robertalexanderylitalo@gmail.com>
2025-01-25 03:46:47 +0200

    Changes 25.1.2025
    - Started adding a new puzzle.
    - Added some assets.

---
+++ Assets/Scripts/Experiment_Door_Switch.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ExperimentDoorSwitch : MonoBehaviour, IInteractable
+{
+    public ExperimentDoor doorController;   // Reference to the DoorController1 instance
+    public AudioSource buttonPressSound;    // Button press sound
+
+    // Implementation of the Interact method from IInteractable
+    public void Interact()
+    {
+        if (doorController != null) // Check if doorController reference is assigned
+        {
+            ButtonPressSound();
+            doorController.ToggleDoor(); // Call ToggleDoor() on the doorController instance
+        }
+    }
+
+    // Play button press sound
+    private void ButtonPressSound()
+    {
+        if (buttonPressSound != null)
+        {
+            buttonPressSound.Play();  // Start playing the button press sound
+        }
+    }
+}
